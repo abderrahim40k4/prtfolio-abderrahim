78f7dd35c3be94a2aacd34a6a04a20bb87745623
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-09-12 10:56:52 +0200

    navBar fenishid

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,15 @@ function App() {
 
   return (
     <>
-          <div>
+          <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-500 selection:text-cyan-900">
+          <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+
+
+          </div>
+            <div className="h-full w-full flixed top-0 -z-0"></div>
+            <div className="container mx-auto px-8"></div>
             <Navbar />
           </div>
-          <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
     </>
   )
 }
