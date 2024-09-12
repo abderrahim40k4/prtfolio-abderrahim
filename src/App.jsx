1f992087fda155ff466f06ba8cc5a1b5c6b43593
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-09-12 11:37:25 +0200

    Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { FaBaby } from "react-icons/fa6"
 import NavBar from "./components/NavBar"
 import Navbar from "./components/NavBar"
+import HeroSec from "./components/HeroSec"
 
 
 function App() {
@@ -8,13 +9,14 @@ function App() {
   return (
     <>
           <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-500 selection:text-cyan-900">
-          <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
-
-
-          </div>
+          <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
             <div className="h-full w-full flixed top-0 -z-0"></div>
-            <div className="container mx-auto px-8"></div>
+            <div className="container mx-auto px-8">
             <Navbar />
+            <HeroSec/>
+
+            </div>
+
           </div>
     </>
   )
--- src/components/HeroSec.jsx
+++ src/components/HeroSec.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import { HERO_CONTENT } from '../constants'
+import pfp from "../assets/kevinRushProfile.jpg"
 const HeroSec = () => {
   return (
-    <div>HeroSec</div>
+    <div className='border-b border-neutral-900 pb-4 lg:mb-36'>
+        <div className="flex flex-wrap">
+            <div className="w-full lg:w-1/2">
+                <div className='flex flex-col items-center lg:items-start'>
+                    <h1 className="mb-16 text-6xl font-thin tracking-tight lg:mb-16 lg:text-8xl">Abderrhim</h1>
+                    <span className='bg-gradient-to-tr from-cyan-500 via-slate-500 to-blue-900 bg-clip-text text-4xl tracking-tight text-transparent'>
+                        Full Stack Developer
+                    </span>
+                    <p className='my-2 max-w-xl py-6 font-light '>{HERO_CONTENT}</p>
+                </div>
+                </div>
+                <div className="w-full lg:w-1/2 lg:p-8">
+                <div className="flex justify-center">
+                    <img src={pfp} alt="pfp"/>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
