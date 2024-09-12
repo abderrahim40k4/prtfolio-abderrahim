98725699879af06e8d7358fafaa780d65b41c70e
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-09-12 11:53:17 +0200

    About section

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import { FaBaby } from "react-icons/fa6"
 import NavBar from "./components/NavBar"
 import Navbar from "./components/NavBar"
 import HeroSec from "./components/HeroSec"
+import AboutSec from "./components/AboutSec"
 
 
 function App() {
 
   return (
     <>
-          <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-500 selection:text-cyan-900">
-          <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+          <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-500 selection:text-cyan-900 bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] ">
+          <div class="fixed top-0 z-[-2] h-full w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
             <div className="h-full w-full flixed top-0 -z-0"></div>
             <div className="container mx-auto px-8">
             <Navbar />
             <HeroSec/>
-
+              <AboutSec/>
             </div>
 
           </div>
---
+++ src/components/AboutSec.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { ABOUT_TEXT } from '../constants'
+import pfp from "../assets/about.jpg"
+
+const AboutSec = () => {
+    return (
+        <div className='border-b border-neutral-900 pb-4'>
+            <h2 className="my-20 text-4xl text-center">About
+                <span className="text-neutral-500 font-medium"> Me</span>
+            </h2>
+            <div className="flex flex-wrap">
+            <div className="w-full lg:w-1/2 lg:p-8">
+                    <div className="flex justify-center">
+                        <img src={pfp} alt="pfp" />
+                    </div>
+                </div>
+                <div className="w-full lg:w-1/2">
+                    <div className='flex flex-col items-center lg:items-start'>
+                        <p className='my-2 max-w-xl py-6 font-light '>{ABOUT_TEXT}</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default AboutSec
